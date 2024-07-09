8f0127a1acfa296e50fa6fee73f23d8ed3d346d0
GitHub Action <action@github.com>
2024-07-09 17:16:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/donslon1/arcade/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/donslon1/arcade/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.donslon1.arcade.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
