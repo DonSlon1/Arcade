69fab3e65d0830a29673bb3452f7d5fd5c00e5b2
DonSlon1 <lukindihel@gmail.com>
2024-07-10 00:32:00 +0200

    fix tests

--- src/test/kotlin/com/github/donslon1/arcade/MyPluginTest.kt
+++ src/test/kotlin/com/github/donslon1/arcade/MyPluginTest.kt
@@ -1,16 +1,10 @@
 package com.github.donslon1.arcade
 
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import kotlin.test.assertEquals
 
 class ArcadePluginTest : BasePlatformTestCase() {
 
     fun testPluginInitialization() {
-        assertEquals(1, 1, "Basic assertion to ensure test framework is working")
-    }
-
-    // Add more dummy tests here
-    fun testDummyFeature() {
-        assertTrue("This is a dummy test") { true }
+        assertEquals( "Basic assertion to ensure test framework is working",1, 1,)
     }
 }
